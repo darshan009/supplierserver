d5c2ac69bc927db8ca2bee0e1dc2c56fb12a2b52
darshan009 <darshansapaliga009@gmail.com>
2016-04-13 04:06:45 +0530

    RESTful

--- server.js
+++ server.js
@@ -7,45 +7,52 @@ var app = express();
 
 //mongoose connection
 mongoose.connect("mongodb://supply:supply123@ds023500.mlab.com:23500/supply");
-//mongoose.connect("mongodb://localhost:27017/supply");
 mongoose.connection.on('error', console.error.bind(console, 'connection error'));
 mongoose.connection.once('open', function callback(){
   console.log("Mongoose connected to mongolab");
 });
 
-//views, bodyparser, cookieParser, session
-app.set("views",__dirname+"/views");
-app.set('view engine', 'jade');
-app.use(express.static(__dirname+'/'));
 
 app.get('/', function(req, res, next){
+  var category = req.query.category;
   var page = parseInt(req.query.page),
      skip = req.param('page') > 0 ? req.param('page') : 0;
-  Supplier.find(null, null, {
+  Supplier.find({category : {$regex : category}}, null, {
      skip: 10 * skip,
      limit: 10
   }, function (err, data) {
+    console.log(data);
      if(err) {
         res.json(500, err);
      }
      else {
-       Supplier.find().exec(function(err, fullList) {
-       var fullListLength = fullList.length/10;
-       console.log(fullListLength);
-        res.render('main', {
-           data: data,
-           fullListLength : fullListLength
-        });
-      });
+        res.send(data);
+      }
+  });
+});
+
+app.get('/supplier', function(req, res, next){
+  var category = req.query.category;
+  var page = parseInt(req.query.page),
+     skip = req.param('page') > 0 ? req.param('page') : 0;
+  Supplier.find({category : {$regex: category}}, null, {
+     skip: 10 * skip,
+     limit: 10
+  }, function (err, data) {
+    console.log(data);
+     if(err) {
+        res.json(500, err);
      }
+     else {
+        res.send(data);
+      }
   });
 });
 
 //each suppliers
-app.get('/suppliers/:id', function(req, res, next) {
-  Supplier.findOne({id : req.params.id}).exec(function(err, found) {
-    console.log(found);
-    res.render('supplierProfile', {found : found});
+app.get('/supplier/:id', function(req, res, next) {
+  Supplier.findOne({id : req.params.id}).exec(function(err, supplier) {
+    res.send(supplier);
   });
 });
 
